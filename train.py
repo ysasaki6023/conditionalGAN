555a6bbf922d9b0f5a293022fde882cfb8391b9f
Yuichi Sasaki <ysasaki6023@gmail.com>
2017-05-04 12:45:49 +0900

    first commit

---
+++ train.py
@@ -0,0 +1,18 @@
+import os,path,sys,shutil
+import numpy as np
+import argparse
+from cgan import BatchGenerator,CGAN
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--nBatch","-b",dest="nBatch",type=int,default=64)
+    parser.add_argument("--learnRate","-r",dest="learnRate",type=float,default=2e-4)
+    parser.add_argument("--saveFolder","-s",dest="saveFolder",type=str,default="models")
+    parser.add_argument("--reload","-l",dest="reload",type=str,default=None)
+    args = parser.parse_args()
+    args.zdim = 100
+
+    batch = BatchGenerator()
+    gan = CGAN(isTraining=True,imageSize=[108,108],args=args)
+
+    gan.train(f_batch=batch.getBatch)
